f186782c9b1f8b050b81b6c644845b5e63428792
ndima-66 <166137534+ndima-66@users.noreply.github.com>
2024-04-05 17:22:26 +0300

    Update HelloWorldModel.java

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/HelloWorldModel.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/HelloWorldModel.java
@@ -36,6 +36,7 @@ import java.util.Optional;
 public class HelloWorldModel {
 
     public static final String PASSWORD="pass";
+    // I added a custom pipeline
 
     @ValueMapValue(name = PROPERTY_RESOURCE_TYPE, injectionStrategy = InjectionStrategy.OPTIONAL)
     @Default(values = "No resourceType")
